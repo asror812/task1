fd4f85d73124c24c1f03b9f101f24017b96ed490
endragon <asror0812@gmail.com>
2026-09-25 18:16:36 +0500

    feat: move on putting role checks from security config to methods with PreAuthorize

--- src/main/java/org/example/order_management/controller/TestController.java
+++ src/main/java/org/example/order_management/controller/TestController.java
@@ -2,6 +2,7 @@ package org.example.order_management.controller;
 
 import org.example.order_management.dto.MessageDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,17 @@ public class TestController {
     @GetMapping("/private/test")
     public ResponseEntity<MessageDto> testPrivate() {
         return ResponseEntity.ok(new MessageDto("Hello World", "from-private", "to-private"));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/api/admin/test")
+    public ResponseEntity<MessageDto> adminHello() {
+        return ResponseEntity.ok(new MessageDto("Доступ разрешён: роль ADMIN", "from-admin", "to-admin"));
+    }
+
+    @PreAuthorize("hasRole('CUSTOMER')")
+    @GetMapping("/api/customer/test")
+    public ResponseEntity<MessageDto> customerHello() {
+        return ResponseEntity.ok(new MessageDto("Доступ разрешён: роль CUSTOMER", "from-customer", "to-customer"));
     }
 }
--- src/main/java/org/example/order_management/security/SecurityConfig.java
+++ src/main/java/org/example/order_management/security/SecurityConfig.java
@@ -3,12 +3,14 @@ package org.example.order_management.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -20,7 +22,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/public/**").permitAll()
-                                .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                                // .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(
                         oauth ->
